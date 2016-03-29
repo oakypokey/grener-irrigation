5762e4ed81113cfbd4be225ed01e31fae7583f93
oakypokey <calvindass@hotmail.com>
2016-03-29 13:12:50 +0400

    Update motortest.py

--- motortest.py
+++ motortest.py
@@ -1,8 +1,16 @@
 import motorsetup
 
 motorsetup.motorforward(50)
+time.sleep(10)
+print("15")
 motorsetup.motorbackward(50)
+time.sleep(10)
+print("30")
 motorsetup.motorforward(100)
+time.sleep(10)
+print("45")
 motorsetup.motorbackward(100)
+time.sleep(10)
+print("Done")
 
 print ("The motor should have moved. If not, check power supply")
