60f4a906172a5c7664295a3c91cc225f1c58bf8f
oakypokey <calvindass@hotmail.com>
2016-03-29 12:03:13 +0400

    Update moisturedetection.py

--- moisturedetection.py
+++ moisturedetection.py
@@ -3,6 +3,7 @@ import time
 import os
 import motorsetup
 
+motorsetup.motorpos = motorpos
 # Open SPI bus
 spi = spidev.SpiDev()
 spi.open(0,0)
@@ -13,13 +14,21 @@ def ReadChannel(channel):
    data = ((adc[1]&3) << 8) + adc[2]
    return data
 
-
 # Main loop - read raw data and display
 while True:
-   soilOne = ReadChannel(2)
+   soilOne = readadc(0)
+   if soilOne < target and motorpos = 0:
+      motorsetup.forward(50)
+   elif soilOne < target and motorpos = 50:
+      time.sleep(10)
+   elif soilOne > target and motorpos = 50:
+      motorsetup.backward(5)
+   elif soilOne > target and motorpos = 0:
+      time.sleep(10)
+   elif soilOne = target and motorpos in (0,50):
+      time.sleep(10)
+   else:
+      break
    
-while soilOne > 500:
-  forward(50)
-  
-while soilOne < 500:
-  backward(50)
+print ("System Error. Motor misalignment. Please recalibrate valve and restart")
+   
