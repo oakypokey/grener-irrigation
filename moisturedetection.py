4ac796f5d3a7e2b9d1c44b6569eea67a84453b8e
oakypokey <calvindass@hotmail.com>
2016-03-29 12:15:20 +0400

    Update moisturedetection.py

--- moisturedetection.py
+++ moisturedetection.py
@@ -4,19 +4,10 @@ import os
 import motorsetup
 
 motorsetup.motorpos = motorpos
-# Open SPI bus
-spi = spidev.SpiDev()
-spi.open(0,0)
-
-# Function to read SPI data from MCP3008 chip
-def ReadChannel(channel):
-   adc = spi.xfer2([1,(8+channel)<<4,0])
-   data = ((adc[1]&3) << 8) + adc[2]
-   return data
 
 # Main loop - read raw data and display
 while True:
-   soilOne = readadc(0)
+   soilOne = mcp3008.readadc(0)
    if soilOne < target and motorpos = 0:
       motorsetup.forward(50)
    elif soilOne < target and motorpos = 50:
