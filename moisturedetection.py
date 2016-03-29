8314f5e0888d847cb65dd4ff9bbc197aa5779a9f
oakypokey <calvindass@hotmail.com>
2016-03-29 12:21:01 +0400

    Update moisturedetection.py

--- moisturedetection.py
+++ moisturedetection.py
@@ -8,13 +8,13 @@ motorsetup.motorpos = motorpos
 # Main loop - read raw data and display
 while True:
    soilOne = mcp3008.readadc(0)
-   if soilOne < target and motorpos = 0:
+   if soilOne < target and motorpos == 0:
       motorsetup.forward(50)
-   elif soilOne < target and motorpos = 50:
+   elif soilOne < target and motorpos == 50:
       time.sleep(10)
-   elif soilOne > target and motorpos = 50:
+   elif soilOne > target and motorpos == 50:
       motorsetup.backward(5)
-   elif soilOne > target and motorpos = 0:
+   elif soilOne > target and motorpos == 0:
       time.sleep(10)
    elif soilOne = target and motorpos in (0,50):
       time.sleep(10)
