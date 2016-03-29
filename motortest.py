5d9b34382a75df187926c3d15d16174ea0a6daa0
oakypokey <calvindass@hotmail.com>
2016-03-29 13:08:10 +0400

    Create motortest.py

---
+++ motortest.py
@@ -0,0 +1,8 @@
+import motorsetup
+
+motorsetup.motorforward(50)
+motorsetup.motorbackward(50)
+motorsetup.motorforward(100)
+motorsetup.motorbackward(100)
+
+print ("The motor should have moved. If not, check power supply")
